0bd75fab8dd2459487f8e01d121dfe7596917603
jimanPark <im@gman.io>
2019-11-06 19:55:46 +0900

    [feature] sanitized html rendering

--- src/app/mobile/pages/fortune/fortune.component.ts
+++ src/app/mobile/pages/fortune/fortune.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-fortune',
@@ -8,13 +9,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FortuneComponent implements OnInit {
   private sampleUrl = '/api/contents/freeunse/dayjiji.nate?jijiPage=3&jiji=00&dateparam=2';
+  public sanitizeHtmlContent;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private sanitizer: DomSanitizer) {
     this.http
     .get(this.sampleUrl, {
       responseType: 'text'
     })
     .subscribe((res) => {
+      this.sanitizeHtmlContent = this.sanitizer.bypassSecurityTrustHtml(res);
       console.log(res);
     });
   }
