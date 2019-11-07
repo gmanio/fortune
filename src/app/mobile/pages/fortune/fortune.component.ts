5aadfc5418a00c0c899aa10f8c0ac83d59e963ed
jimanPark <im@gman.io>
2019-11-07 23:21:45 +0900

    [feature] added sanitize.css

--- src/app/mobile/pages/fortune/fortune.component.ts
+++ src/app/mobile/pages/fortune/fortune.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-fortune',
@@ -10,20 +11,34 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class FortuneComponent implements OnInit {
   private sampleUrl = '/api/contents/freeunse/dayjiji.nate?jijiPage=3&jiji=00&dateparam=2';
   public sanitizeHtmlContent;
+  public fortuneData$ = new BehaviorSubject(null);
 
   constructor(private http: HttpClient,
               private sanitizer: DomSanitizer) {
-    this.http
-    .get(this.sampleUrl, {
-      responseType: 'text'
-    })
-    .subscribe((res) => {
-      this.sanitizeHtmlContent = this.sanitizer.bypassSecurityTrustHtml(res);
-      console.log(res);
-    });
+    // this.http
+    // .get(this.sampleUrl, {
+    //   responseType: 'text'
+    // })
+    // .subscribe((res) => {
+    //   this.sanitizeHtmlContent = this.sanitizer.bypassSecurityTrustHtml(res);
+    //   console.log(res);
+    // });
   }
 
   ngOnInit() {
+  }
+
+  fetchData() {
+    const fetch$ = this.http
+    .get(this.sampleUrl, {
+      responseType: 'text'
+    });
+    this.fortuneData$.next(fetch$);
+  }
+
+  getFortuneData() {
+    this.fetchData();
+    console.log('getFortuneData');
   }
 
 }
